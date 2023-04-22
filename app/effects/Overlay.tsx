f08f578ce150431168c0fa22280444de02d78c01
Paul Cowan <paul.cowan@cutting.scot>
2023-04-22 22:14:57 +0100

    remove framer-motion

--- app/effects/Overlay.tsx
+++ app/effects/Overlay.tsx
@@ -2,8 +2,8 @@ import { motion } from 'framer-motion';
 import { Nav } from '~/Nav/Top';
 import { CTAButton } from '~/components/CTAButton/CTAButton';
 import Lenis from '@studio-freight/lenis';
-import { useCallback, useEffect, useRef } from 'react';
-import gsap from 'gsap';
+import { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
+import gsap, { Bounce } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 
@@ -15,173 +15,89 @@ const lenisStub = {
   stop() {},
 };
 
-let lenis =
-  typeof window === 'undefined'
-    ? lenisStub
-    : new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -5 * t)),
-        direction: 'vertical',
-        gestureDirection: 'vertical',
-        mouseMultiplier: 1,
-      });
+// let lenis =
+//   typeof window === 'undefined'
+//     ? lenisStub
+//     : new Lenis({
+//         duration: 1.2,
+//         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -5 * t)),
+//         direction: 'vertical',
+//         gestureDirection: 'vertical',
+//         mouseMultiplier: 1,
+//       });
 
 export function Overlay() {
   const rafID = useRef<number>();
   const overlay = useRef<HTMLDivElement>(null);
   const works = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const heading = useRef<HTMLHeadingElement>(null);
+  const cta = useRef<HTMLSpanElement>(null);
 
-  const raf = useCallback((time: number) => {
-    lenis.raf(time);
-    rafID.current = requestAnimationFrame(raf);
+  useLayoutEffect(() => {
+    gsap.fromTo(
+      heading.current,
+      { translateX: '-100%', opacity: 0 },
+      { translateX: '0%', opacity: 1, duration: 1.8, delay: 0.5 },
+    );
+
+    gsap.fromTo(
+      buttonRef.current,
+      {
+        translateX: '-120%',
+        opacity: 0,
+      },
+      {
+        translateX: '0%',
+        opacity: 1,
+        delay: 2.6,
+      },
+    );
+
+    gsap.fromTo(
+      cta.current,
+      {
+        translateX: '-120%',
+        opacity: 0,
+      },
+      {
+        translateX: '0%',
+        opacity: 1,
+        delay: 2.6,
+      },
+    );
+
+    gsap.fromTo(
+      'header',
+      {
+        y: 60,
+        opacity: 0,
+      },
+      { y: 0, opacity: 1, delay: 2, ease: Bounce.easeOut, duration: 2 },
+    );
   }, []);
-
-  useEffect(() => {
-    if (typeof lenis.start === 'undefined') {
-      return;
-    }
-
-    lenis.start();
-  }, []);
-
-  useEffect(() => {
-    rafID.current = requestAnimationFrame(raf);
-
-    return () => {
-      if (!rafID.current) {
-        return;
-      }
-
-      cancelAnimationFrame(rafID.current);
-
-      lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -5 * t)),
-        direction: 'vertical',
-        gestureDirection: 'vertical',
-        mouseMultiplier: 1,
-      });
-
-      lenis.stop();
-    };
-  }, [raf]);
-
-  useEffect(() => {
-    if (!overlay.current) {
-      return;
-    }
-
-    if (!works.current) {
-      return;
-    }
-
-    // const panels = [overlay.current, works.current];
-    // panels.forEach((panel, index) => {
-    //   ScrollTrigger.create({
-    //     trigger: panel,
-    //     start: 'top top',
-    //     end: 'bottom top',
-    //     // pin: true,
-    //     // pinSpacing: false,
-    //     // snap: {
-    //     //   snapTo: 1,
-    //     //   duration: { min: 0.3, max: 0.7 },
-    //     //   ease: 'power2.inOut',
-    //     //   delay: 0,
-    //     // },
-    //     // markers: true,
-    //   });
-    // });
-  });
-
-  // useEffect(() => {
-  //   if (!works.current) {
-  //     return;
-  //   }
-
-  //   let worksST = ScrollTrigger.create({
-  //     trigger: works.current,
-  //     start: 'top top',
-  //   });
-
-  //   ScrollTrigger.create({
-  //     trigger: works.current,
-  //     start: 'top center',
-  //     end: 'bottom center',
-  //   });
-
-  //   if (!buttonRef.current) {
-  //     return;
-  //   }
-
-  //   const scroll = (e: MouseEvent) => {
-  //     e.preventDefault();
-  //     gsap.to(window, { duration: 1, scrollTo: worksST.start, overwrite: 'auto' });
-  //   };
-
-  //   buttonRef.current.addEventListener('click', scroll);
-
-  //   const button = buttonRef.current;
-
-  //   return () => {
-  //     button?.removeEventListener('click', scroll);
-  //   };
-  // }, []);
 
   return (
     <>
-      <motion.header
-        className="flex justify-between pt-10 pb-10"
-        animate={{ y: 0, opacity: 1 }}
-        initial={{ y: 60, opacity: 0 }}
-        transition={{
-          type: 'spring',
-          stiffness: 200,
-          damping: 6,
-          delay: 4,
-        }}
-      >
-        <motion.div>
+      <header className="flex justify-between pt-10 pb-10">
+        <div>
           <Nav />
-        </motion.div>
-        <motion.span
-          animate={{ translateX: '0%', opacity: 1 }}
-          initial={{ translateX: '-120%', opacity: 0 }}
-          transition={{
-            delay: 2.6,
-          }}
-        >
+        </div>
+        <span ref={cta}>
           <CTAButton />
-        </motion.span>
-      </motion.header>
+        </span>
+      </header>
       <main>
-        <section ref={overlay} className="overlay grid place-content-center h-full">
-          <motion.h1
-            animate={{ translateX: '0%', opacity: 1 }}
-            initial={{ translateX: '-100%', opacity: 0 }}
-            className="text-9xl"
-            transition={{
-              duration: 1.8,
-              delay: 2.5,
-            }}
-          >
+        <section ref={overlay} className="grid place-content-center h-full">
+          <h1 ref={heading} className="text-9xl">
             STRUGGLING TO DELIVER FRONTEND FEATURES?
-          </motion.h1>
-          <motion.button
-            animate={{ translateX: '0%', opacity: 1 }}
-            initial={{ translateX: '-120%', opacity: 0 }}
-            transition={{
-              delay: 2.6,
-            }}
+          </h1>
+          <button
             className="ctaButton border-2 border-white py-5 px-10 rounded-full rubik text-4xl mt-20 place-self-center"
-            // onClick={() => {
-            //   window.open('https://andersonmancini.dev', 'tab');
-            // }}
             ref={buttonRef}
           >
             FIND OUT HOW WE CAN HELP
-          </motion.button>
+          </button>
         </section>
         <section ref={works} className="works h-full">
           <h3>RECENT WORKS</h3>
